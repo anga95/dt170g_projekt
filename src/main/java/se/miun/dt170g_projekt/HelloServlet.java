bd0629f16f2b91f102e44868ced80a364ec97031
Hagman01 <wille.hagman@gmail.com>
2023-02-08 10:35:15 +0100

    test

--- src/main/java/se/miun/dt170g_projekt/HelloServlet.java
+++ src/main/java/se/miun/dt170g_projekt/HelloServlet.java
@@ -6,10 +6,11 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private String message, message2;
 
     public void init() {
         message = "Hello World!";
+        message2 = "Helloooo ";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -19,7 +20,9 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<p>"  + message2 + "</p>");
         out.println("</body></html>");
+
     }
 
     public void destroy() {
