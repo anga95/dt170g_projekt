ee333adb5e2c2ab95fbfa3a212b8f15bc0f7b91f
Svante Allgulander <svanteallgulander@gmail.com>
2023-02-15 12:00:28 +0100

    La till några nya aktiviter och fixat så att man kan klicka vidare sig till tables både via dagens lunch och a la carte

--- AndroidApp/app/src/main/java/com/example/ordersystem/PersonalPage1.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/PersonalPage1.java
@@ -2,13 +2,28 @@ package com.example.ordersystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PersonalPage1 extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_page1);
+
+        button1 = (Button)findViewById(R.id.dagensLunchBtn);
+        button1.setOnClickListener(view -> {
+            Intent intent = new Intent(PersonalPage1.this, Tables.class);
+            startActivity(intent);
+        });
+
+        button1 = (Button)findViewById(R.id.aLaCarteBtn);
+        button1.setOnClickListener(view -> {
+            Intent intent = new Intent(PersonalPage1.this, Tables.class);
+            startActivity(intent);
+        });
     }
 }
