2b0058e7cec58e1ebdbc5337a295686808f2af9e
Svante Allgulander <svanteallgulander@gmail.com>
2023-02-16 15:25:43 +0100

    La till lite grejer för tables

--- AndroidApp/app/src/main/java/com/example/ordersystem/TablesLunch.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/TablesLunch.java
@@ -2,22 +2,56 @@ package com.example.ordersystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class TablesLunch extends AppCompatActivity {
 
-    Button button1;
+    //Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tables_lunch);
 
-        button1 = (Button)findViewById(R.id.table1Btn);
-        button1.setOnClickListener(view -> {
-            Intent intent = new Intent(TablesLunch.this, addLunch.class);
-            startActivity(intent);
-        });
+    }
+
+    public void nextWindow(String value){
+        Intent intent = new Intent(TablesLunch.this, addLunch.class);
+        intent.putExtra("message", value);
+        startActivity(intent);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void helpB(View view) {
+        Button clickedButton = (Button) view;
+        switch(clickedButton.getId()){
+            case R.id.table1Btn:
+                nextWindow("Table 1");
+                break;
+            case R.id.table2Btn:
+                nextWindow("Table 2");
+                break;
+            case R.id.table3Btn:
+                nextWindow("Table 3");
+                break;
+            case R.id.table4Btn:
+                nextWindow("Table 4");
+                break;
+            case R.id.table5Btn:
+                nextWindow("Table 5");
+                break;
+            case R.id.table6Btn:
+                nextWindow("Table 6");
+                break;
+            case R.id.table7Btn:
+                nextWindow("Table 7");
+                break;
+            case R.id.table8Btn:
+                nextWindow("Table 8");
+                break;
+        }
     }
 }
--- AndroidApp/app/src/main/java/com/example/ordersystem/addLunch.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/addLunch.java
@@ -3,13 +3,16 @@ package com.example.ordersystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class addLunch extends AppCompatActivity {
+    TextView placeholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lunch);
-
+        placeholder = (TextView)findViewById(R.id.tableNum);
+        placeholder.setText(getIntent().getStringExtra("message"));
     }
 }
