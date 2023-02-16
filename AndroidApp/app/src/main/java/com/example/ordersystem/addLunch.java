0daf84e2a22285c0116c72a208856bc51462b32b
Svante Allgulander <svanteallgulander@gmail.com>
2023-02-16 16:49:08 +0100

    Added dropdown menu

--- AndroidApp/app/src/main/java/com/example/ordersystem/addLunch.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/addLunch.java
@@ -3,7 +3,10 @@ package com.example.ordersystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class addLunch extends AppCompatActivity {
     TextView placeholder;
@@ -14,5 +17,10 @@ public class addLunch extends AppCompatActivity {
         setContentView(R.layout.activity_add_lunch);
         placeholder = (TextView)findViewById(R.id.tableNum);
         placeholder.setText(getIntent().getStringExtra("message"));
+
+        Spinner spinnerCourses=findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence>adapter= ArrayAdapter.createFromResource(this, R.array.courses, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerCourses.setAdapter(adapter);
     }
 }
