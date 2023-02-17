ad39706109c36be34344c6ac99fd1af00d1c7c4f
anga95 <andreas.gabrielsson@live.se>
2023-02-17 14:17:31 +0100

    deleted some stupid shit to get a fresh fucking start

--- src/main/java/entity/DDL/APP/RESTAURANT.sql
+++
@@ -1,9 +0,0 @@
-create table RESTAURANT
-(
-    ID        INTEGER default AUTOINCREMENT: start 1 increment 1 generated always as identity
-        constraint ID
-        primary key,
-    NAME      VARCHAR(50),
-    TELEPHONE VARCHAR(50)
-);
-
