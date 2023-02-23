6f2ea82540289d1503286c87af1812fc5ee0c8eb
wibe2001 <wibe2001@student.miun.se>
2023-02-23 14:25:44 +0100

    Test Order

--- AndroidApp/app/src/main/java/com/example/ordersystem/ChefPage.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/ChefPage.java
@@ -1,18 +1,85 @@
 package com.example.ordersystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChefPage extends AppCompatActivity {
 
-    Button button1;
+    private RecyclerView orderlist;
+    private OrderAdapter orderAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_page);
 
+        orderlist = findViewById(R.id.order_list);
 
+        orderAdapter = new OrderAdapter(this, getOrderSample());
+        }
+
+        private List<Order> getOrderSample() {
+            List<Order> ordersamples = new ArrayList<>();
+
+            Order ordernr1 = new Order(1, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), false, false, false);
+            ordernr1.setTableNr(1);
+            ordernr1.getStarter().add("Tomatsoppa");
+            ordernr1.getMainCourse().add("Köttbullar");
+            ordernr1.getDessert().add("Bakelse");
+            ordersamples.add(ordernr1);
+
+
+
+            return ordersamples;
+        }
+
+
+
+
+
+    private class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
+
+        private final Context context;
+        private final List<Order> orders;
+
+        public OrderAdapter(Context context, List<Order> orders) {
+            this.context = context;
+            this.orders = orders;
+        }
+
+        public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+            View view1 = LayoutInflater.from(context).inflate(R.layout.order_item, parent, false);
+            return new OrderViewHolder(view1);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull OrderAdapter.OrderViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return orders.size();
+        }
+
+        public class OrderViewHolder extends RecyclerView.ViewHolder{
+
+
+            public OrderViewHolder(View viewItem){
+                super(viewItem);
+            }
+        }
     }
 }
