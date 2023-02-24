fbaf3be13898081e3d84e249b9afaf21e90d7d8d
Svante Allgulander <svanteallgulander@gmail.com>
2023-02-24 13:37:01 +0100

    Fixat en funktionell dagens lunch sida

--- AndroidApp/app/src/main/java/com/example/ordersystem/addLunch.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/addLunch.java
@@ -1,27 +1,30 @@
 package com.example.ordersystem;
 
-import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class addLunch extends AppCompatActivity {
     TextView placeholder;
-    ArrayList<String> listItems = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
+    String[] foods = {"Hamburger", "Fisk med pinnar", "Lasagne med ädelostsås och ruccola"};
+    String[] drinks = {"Vatten", "Coca-Cola", "Fanta"};
+    ListView listView;
+    Button addButton;
 
+    private final Set<String> selectedItems = new HashSet<>();
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -29,63 +32,25 @@ public class addLunch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lunch);
 
-        placeholder = (TextView)findViewById(R.id.tableNum);
+        placeholder = findViewById(R.id.tableNum);
         placeholder.setText(getIntent().getStringExtra("message"));
 
-        placeholder = (TextView)findViewById(R.id.lunchOne);
-        placeholder.setText("Hamburgare");
-        placeholder = (TextView)findViewById(R.id.lunchTwo);
-        placeholder.setText("Fisk med pinnar");
-        placeholder = (TextView)findViewById(R.id.lunchThree);
-        placeholder.setText("Lasagne med ädelostsås och ruccola");
+        List<String> wholeMenu = Arrays.asList("Hamburgare", "Fisk med pinnar", "Lasagne med ädelostsås och ruccola", "Vatten", "Coca-Cola", "Fanta");
+        List<String> drinks = Arrays.asList("Vatten", "Coca-Cola", "Fanta");
+        List<String> foods = Arrays.asList("Hamburgare", "Fisk med pinnar", "Lasagne med ädelostsås och ruccola");
 
-        placeholder = (TextView)findViewById(R.id.drinkOne);
-        placeholder.setText("Vatten");
-        placeholder = (TextView)findViewById(R.id.drinkTwo);
-        placeholder.setText("Coca-Cola");
-        placeholder = (TextView)findViewById(R.id.drinkThree);
-        placeholder.setText("Fanta");
+        ListView destinationList = findViewById(R.id.totalOrderList);
+        DestinationListAdapter destinationListAdapter = new DestinationListAdapter(this, R.layout.totalorder_listview, wholeMenu, selectedItems);
+        destinationList.setAdapter(destinationListAdapter);
 
-        ListView listView = findViewById(R.id.totalOrder);
-        //ArrayList<String> items = new ArrayList<>();
-        //items.add("Item 1");
-        //CustomListAdapter adapter = new CustomListAdapter(this,items);
-        //listView.setAdapter(adapter);
+        ListView sourceList = findViewById(R.id.foodsList);
+        SourceListAdapter sourceListAdapter = new SourceListAdapter(this, R.layout.custom_listview, foods, destinationListAdapter);
+        sourceList.setAdapter(sourceListAdapter);
 
-        ArrayList<String> list = new ArrayList<String>(Arrays.asList("111,222,333,444".split(",")));
-        listView.setAdapter(new CustomListAdapter(list,context));
+        sourceList = findViewById(R.id.drinksList);
+        sourceListAdapter = new SourceListAdapter(this, R.layout.custom_listview, drinks, destinationListAdapter);
+        sourceList.setAdapter(sourceListAdapter);
 
-        //list = (ListView) findViewById(R.id.totalOrder);
-        //list.setAdapter(new CustomListAdapter(list, context));
-        //adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
-        //list.setAdapter(adapter);
-
-
-    }
-
-    public void updateTotal(int resource){
-        placeholder = findViewById(resource);
-        String value = (String) placeholder.getText();
-        listItems.add(value + "      x" + 0);
-        adapter.notifyDataSetChanged();
-    }
-
-    public void pressedButton(View view){
-        Button clickedButton = (Button) view;
-        TextView item;
-        switch (clickedButton.getId()){
-            case R.id.addLunchOneBtn:
-                updateTotal(R.id.lunchOne);
-                break;
-            case R.id.addLunchTwoBtn:
-                updateTotal(R.id.lunchTwo);
-                break;
-            case R.id.addOrderBtn:
-                addToCart();
-        }
-    }
-
-    private void addToCart(){
 
     }
 
