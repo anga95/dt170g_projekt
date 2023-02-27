37395bb752f733e47b6e0c305412400f16852d50
Svante Allgulander <svanteallgulander@gmail.com>
2023-02-27 14:47:02 +0100

    Added new module ChefApp

--- AndroidApp/app/src/main/java/com/example/ordersystem/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/MainActivity.java
@@ -9,22 +9,15 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     //test
     Button button1;
-    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = (Button)findViewById(R.id.personalBtn);
+        button1 = findViewById(R.id.personalBtn);
         button1.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, PersonalPage1.class);
             startActivity(intent);
-        });
-
-        button2 = (Button)findViewById(R.id.kockBtn);
-        button2.setOnClickListener(view -> {
-            Intent intent2 = new Intent(MainActivity.this, ChefPage.class);
-            startActivity(intent2);
         });
     }
 }
