ebc3b0a0d266a4157c0bd0b937266e3418e29f0e
Svante Allgulander <svanteallgulander@gmail.com>
2023-02-27 14:26:00 +0100

    Uppdaterade lite i dagens lunch och fixade mer till A La Carte sidan

--- AndroidApp/app/src/main/java/com/example/ordersystem/ChefPage.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/ChefPage.java
@@ -3,10 +3,13 @@ package com.example.ordersystem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.Placeholder;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
@@ -17,6 +20,10 @@ import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,14 +38,52 @@ public class ChefPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customchefpageadapter);
 
-        placeholder = findViewById(R.id.textView8);
-        placeholder.setText(getIntent().getStringExtra("message"));
+        placeholder = findViewById(R.id.textView5);
+        //placeholder.setText(getIntent().getStringExtra("message"));
 
         LinearLayout oneOrder = findViewById(R.id.mainRecylcler);
         OrderAdapter orderAdapter1 = new OrderAdapter(this, getOrderSample());
         //orderlist = findViewById(R.id.order_list);
 
         orderAdapter = new OrderAdapter(this, getOrderSample());
+
+        // Start a new thread to listen for data from the sender AVD
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Create a server socket to listen for connections from the sender AVD
+                    ServerSocket serverSocket = new ServerSocket(5000); // Replace "1234" with the port number you want to use
+
+                    // Accept a connection from the sender AVD
+                    Socket socket = serverSocket.accept();
+
+                    // Get the InputStream of the socket
+                    InputStream inputStream = socket.getInputStream();
+
+                    // Read the data from the InputStream
+                    byte[] buffer = new byte[1024];
+                    int length = inputStream.read(buffer);
+                    final String data = new String(buffer, 0, length);
+
+                    // Update the UI with the received data
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            placeholder.setText(data);
+                        }
+                    });
+
+                    // Close the InputStream, the socket, and the server socket
+                    inputStream.close();
+                    socket.close();
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
         }
 
         private List<Order> getOrderSample() {
--- AndroidApp/app/src/main/java/com/example/ordersystem/PersonalPage1.java
+++ AndroidApp/app/src/main/java/com/example/ordersystem/PersonalPage1.java
@@ -1,11 +1,18 @@
 package com.example.ordersystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class PersonalPage1 extends AppCompatActivity {
 
@@ -17,11 +24,40 @@ public class PersonalPage1 extends AppCompatActivity {
 
         button1 = findViewById(R.id.dagensLunchBtn);
         button1.setOnClickListener(view -> {
-            //Intent intent = new Intent(PersonalPage1.this, TablesLunch.class);
+
+            //ComponentName componentName = new ComponentName("com.example.ordersystem", "com.example.ordersystem.ChefPage");
+            // Create an intent with the component name
+            //Intent intent = new Intent();
+            //intent.setComponent(componentName);
+
+            // Start the activity on the other AVD
             //startActivity(intent);
-            Intent intent = new Intent(PersonalPage1.this, ChefPage.class);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        // Create a socket to connect to the receiver AVD
+                        Socket socket = new Socket("10.0.2.2", 5000); // Replace "1234" with the port number you want to use
+
+                        // Get the OutputStream of the socket
+                        OutputStream outputStream = socket.getOutputStream();
+
+                        // Write the data you want to send to the OutputStream
+                        String data = "Hello, receiver!";
+                        outputStream.write(data.getBytes());
+
+                        // Close the OutputStream and the socket
+                        outputStream.close();
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+            //startActivity(intent);
+            /*Intent intent = new Intent(PersonalPage1.this, ChefPage.class);
             intent.putExtra("message", "Det här gick ju bra");
-            startActivity(intent);
+            startActivity(intent);*/
 
         });
 
