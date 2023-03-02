3fd1a7e22bf0bd514cd35e87b7fc916f8608b0a3
Nordin Suleimani <75100826+Nordin-S@users.noreply.github.com>
2023-03-02 19:44:59 +0100

    Trying to do RESTful api calls

--- Modules/scheduleapp/app/src/main/java/se/miun/dt170g/scheduleapp/LoginActivity.java
+++ Modules/scheduleapp/app/src/main/java/se/miun/dt170g/scheduleapp/LoginActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String ANTONS_SKAFFERI_URL = "http://10.0.2.2:8080/AntonsSkafferi/api";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,43 +32,89 @@ public class LoginActivity extends AppCompatActivity {
         View view = inflator.inflate(R.layout.custom_title, null);
         actionBar.setCustomView(view);
 
-// Get the UUID of the device
-        String uuid = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        // Get the UUID of the device
+        Button identifyButton = findViewById(R.id.buttonIdentifyEmail);
+        identifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uuid = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+                String email = findViewById(R.id.editTextUserEmail).toString();
 
-        System.out.println("our uuid " + uuid);
+                // Check if the UUID is in the database
+                if (isEmailInDatabase(email, uuid)) {
+                    // If the UUID is in the database, retrieve the user info
+                    int empId = getUserInfoByUuid(uuid);
 
-// Check if the UUID is in the database
-        if (isUuidInDatabase(uuid)) {
-            // If the UUID is in the database, retrieve the user info
-            boolean userInfo = getUserInfoByUuid(uuid);
+                    // Save the user info in shared preferences
+                    SharedPreferences prefs = getSharedPreferences("appPrefs", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("empId", String.valueOf(empId)); //empId.getUsername()
+                    editor.apply();
 
-            // Save the user info in shared preferences
-            SharedPreferences prefs = getSharedPreferences("appPrefs", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString("userIdentified", "1"); //userInfo.getUsername()
-            editor.apply();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage("You are now logged in. Continue to schedule page?");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // user clicked OK, continue with operation
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            // Change view to ViewScheduleActivity
+                            startActivity(intent);
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // user cancelled the dialog, do nothing
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
 
-            // Change view to ViewScheduleActivity
-            Intent intent = new Intent(this, ViewSchduleActivity.class);
-            startActivity(intent);
-        } else {
-            // If the UUID is not in the database, prompt the user to contact an admin
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("User not found");
-            builder.setMessage("Please contact an admin to add your account to the database.");
-            builder.setPositiveButton("OK", null);
-            builder.show();
-        }
+                } else {
+                    // If the email is not in the database, prompt the user to contact an admin
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setTitle("User not found");
+                    builder.setMessage("Please contact the manager to register your account.");
+                    builder.setPositiveButton("OK", null);
+                    builder.show();
+                }
+            }
+        });
 
     }
 
-    private boolean getUserInfoByUuid(String uuid) {
+    private int getUserInfoByUuid(String uuid) {
         // api call to get user info by uuid
-        return true;
+        String url = ANTONS_SKAFFERI_URL + "/test";
+        HttpUtils httpUtils = new HttpUtils();
+
+        String result;
+        try {
+            result = httpUtils.execute(url).get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (result.equals("Hello World")) {
+            return 1;
+        }
+        return -1;
     }
 
-    private boolean isUuidInDatabase(String uuid) {
-        // api call to check if uuid is in database
-        return false;
+    private boolean isEmailInDatabase(String email, String uuid) {
+        // api call to check if email is in database
+
+        // api call to get user info by uuid
+        String url = ANTONS_SKAFFERI_URL + "/verify-emp?email=" + email + "&uuid=" + uuid;
+        HttpUtils httpUtils = new HttpUtils();
+
+        String result;
+        try {
+            result = httpUtils.execute(url).get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (result.equals("Hello World")) {
+            return true;
+        }
+        return true;
     }
 }
