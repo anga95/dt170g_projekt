61e50edce937ef0d1803493f4b9a5b1bbe5ed73d
anga95 <andreas.gabrielsson@live.se>
2023-03-03 14:01:09 +0100

    testing dao

--- src/main/java/se/miun/dt170g_projekt/api_resources/alacarteResource.java
+++ src/main/java/se/miun/dt170g_projekt/api_resources/alacarteResource.java
@@ -1,15 +1,33 @@
-package se.miun.dt170g_projekt.api_resources;
-
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
-public class alacarteResource {
-
-        @POST
-        @Produces(MediaType.APPLICATION_JSON)
-        public String alacarte() {
-            return "Hello World";
-        }
-}
+//package martin_test.deeper.web;
+//
+//import jakarta.inject.Inject;
+//import jakarta.ws.rs.*;
+//import jakarta.ws.rs.core.MediaType;
+//import martin_test.deeper.entities.*;
+//
+//import java.util.List;
+//
+//@Path("/AlaCarte")
+//public class AlaCarteResource {
+//
+//    @Inject AlaCarteBean alaCarteBean;
+//
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<AlaCarte> getAlaCarte(){
+//        return alaCarteBean.getAlaCarte();
+//    }
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public AlaCarte create(AlaCarte alc){
+//        return alaCarteBean.create(alc);
+//    }
+//
+//    @DELETE
+//    public void delete(int id){
+//        alaCarteBean.deleteCarte(id);
+//    }
+//
+//}
