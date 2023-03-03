db541d840628a0383a6f434f5a93eb6d7098722d
Nordin Suleimani <75100826+Nordin-S@users.noreply.github.com>
2023-03-03 13:26:16 +0100

    Commented files temporaily

--- src/main/java/se/miun/dt170g_projekt/api_resources/AlaCarteResource.java
+++ src/main/java/se/miun/dt170g_projekt/api_resources/AlaCarteResource.java
@@ -1,33 +1,33 @@
-package martin_test.deeper.web;
-
-import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import martin_test.deeper.entities.*;
-
-import java.util.List;
-
-@Path("/AlaCarte")
-public class AlaCarteResource {
-
-    @Inject AlaCarteBean alaCarteBean;
-
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<AlaCarte> getAlaCarte(){
-        return alaCarteBean.getAlaCarte();
-    }
-
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public AlaCarte create(AlaCarte alc){
-        return alaCarteBean.create(alc);
-    }
-
-    @DELETE
-    public void delete(int id){
-        alaCarteBean.deleteCarte(id);
-    }
-
-}
+//package martin_test.deeper.web;
+//
+//import jakarta.inject.Inject;
+//import jakarta.ws.rs.*;
+//import jakarta.ws.rs.core.MediaType;
+//import martin_test.deeper.entities.*;
+//
+//import java.util.List;
+//
+//@Path("/AlaCarte")
+//public class AlaCarteResource {
+//
+//    @Inject AlaCarteBean alaCarteBean;
+//
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<AlaCarte> getAlaCarte(){
+//        return alaCarteBean.getAlaCarte();
+//    }
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public AlaCarte create(AlaCarte alc){
+//        return alaCarteBean.create(alc);
+//    }
+//
+//    @DELETE
+//    public void delete(int id){
+//        alaCarteBean.deleteCarte(id);
+//    }
+//
+//}
