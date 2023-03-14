00bae885006f3b35cbdec6c124cc8bd975a9d8f5
Svante Allgulander <svanteallgulander@gmail.com>
2023-03-14 13:35:19 +0100

    la till allt till sql.sql

--- src/main/java/se/miun/dt170g_projekt/entities/sql.sql
+++ src/main/java/se/miun/dt170g_projekt/entities/sql.sql
@@ -81,6 +81,15 @@ create table APP.daily_lunch
     PRICE   INTEGER,
     TIME    INTEGER
 );
+
+insert into APP.daily_lunch (weekday, lunch_1, lunch_2, lunch_3, price, time)
+values
+    ('monday', 'Pyttipanna (stekt potatis med lök och kött)', 'Fish and chips med remouladsås', 'Vegetarisk lasagne', 90, 15),
+    ('tuesday','Köttbullar med potatismos och lingonsylt', 'Köttfärslimpa med potatismos och lingonsylt', 'Vegetarisk lasagne', 90, 20),
+    ('wednesday', 'Grillad kyckling med rostade rotfrukter','Stekt strömming med potatismos och lingon' ,'Vegetarisk quinoasallad med fetaost och rödbetor', 90, 25),
+    ('thursday', 'Fatimas skogs biff med nyodlade potatisar och brunsås ', 'Lax panett med rotmos och grönsaker', 'Vegetarisk bergshamburgare med tillbehör', 90, 20),
+    ('friday', 'Köttfärssås med pasta', 'Köttfärslimpa med potatismos och lingonsylt', 'Vegetarisk lasagne', 90, 30);
+
 create table APP.booking
 (
     id      INTEGER generated always as identity
@@ -94,5 +103,73 @@ create table APP.booking
     email varchar(25),
     number_of_people varchar(25),
     extra varchar(25)
+);
+
+
+CREATE TABLE APP.MENU_ITEMS (
+                                ID INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                                PRICE INTEGER NOT NULL,
+                                NAME VARCHAR(100),
+                                DESCRIPTION VARCHAR(100),
+                                TIME INTEGER,
+                                CATEGORY VARCHAR(50) CHECK(CATEGORY IN ('Drinks', 'Dessert', 'Starters', 'MainCourse', 'Lunch'))
+);
+
+create table APP.ORDER_ITEMS
+(
+    ID INTEGER GENERATED ALWAYS AS IDENTITY constraint "ORDER_ITEMS_pk" primary key,
+    MENU_ITEM_ID INTEGER constraint MENU_ITEM_ID references MENU_ITEMS,
+    STATUS BOOLEAN,
+    QUANTITY INTEGER,
+    TABLE_NR INTEGER
+);
+
+
+INSERT INTO APP.MENU_ITEMS (PRICE, NAME, DESCRIPTION, TIME, CATEGORY) VALUES
+                                                                          (30, 'French fries', 'Crispy fries with classic ketchup', 10, 'Starters'),
+                                                                          (40, 'Grilled asparagus', 'Tender asparagus with a zesty lemon butter', 15, 'Starters'),
+                                                                          (20, 'Mashed potatoes', 'Smooth and buttery mashed potatoes', 15, 'Starters'),
+                                                                          (100, 'Chicken', 'Juicy chicken with a side of vegetables', 30, 'MainCourse'),
+                                                                          (80, 'Spaghetti', 'Traditional pasta dish with rich tomato sauce', 25, 'MainCourse'),
+                                                                          (120, 'Tenderloin steak', 'Tender steak with creamy mushroom sauce', 35, 'MainCourse'),
+                                                                          (40, 'Chocolate chip cookie', 'Classic cookie with melty chocolate chips', 10, 'Dessert'),
+                                                                          (60, 'Vanilla ice cream', 'Rich and creamy vanilla ice cream', 10, 'Dessert'),
+                                                                          (30, 'Soda', 'Refreshing soda with unlimited refills', 5, 'Drinks'),
+                                                                          (40, 'Orange juice', 'Fresh and tangy orange juice', 10, 'Drinks'),
+                                                                          (50, 'Beer', 'Cold and refreshing beer', 15, 'Drinks'),
+                                                                          (60, 'Red wine', 'Rich and bold red wine', 20, 'Drinks'),
+                                                                          (70, 'White wine', 'Crisp and refreshing white wine', 20, 'Drinks'),
+                                                                          (80, 'Margarita', 'Classic tequila cocktail with a salted rim', 10, 'Drinks'),
+                                                                          (50, 'Caesar salad', 'Fresh greens and tender chicken in a tangy dressing', 15, 'Lunch'),
+                                                                          (70, 'Mixed greens', 'Light and refreshing salad with a balsamic twist', 10, 'Lunch'),
+                                                                          (100, 'Club sandwich', 'Hearty sandwich with turkey, bacon, and all the fixings', 20, 'Lunch'),
+                                                                          (90, 'Cheeseburger', 'Juicy burger with melty cheese and crispy fries', 15, 'Lunch'),
+                                                                          (60, 'Margherita pizza', 'Traditional pizza with fresh mozzarella and tomato sauce', 25, 'Lunch'),
+                                                                          (30, 'Tomato soup', 'Comforting soup with crunchy croutons', 10, 'Starters');
+
+
+INSERT INTO APP.ORDER_ITEMS (MENU_ITEM_ID, QUANTITY, TABLE_NR)
+VALUES
+    (1, 1, 1),
+    (1, 3, 2),
+    (1, 6, 2),
+    (2, 2, 3),
+    (2, 5, 8),
+    (2, 9, 1),
+    (6, 3, 1),
+    (3, 4, 2),
+    (3, 7, 3),
+    (3, 9, 1);
+
+create table APP.TOTAL_ORDERS
+(
+    ID INTEGER GENERATED ALWAYS AS IDENTITY constraint "TOTAL_ORDERS_pk" primary key,
+    NAME         VARCHAR(250),
+    QUANTITY     INTEGER,
+    NOTE         VARCHAR(250),
+    STATUS       BOOLEAN default FALSE,
+    CATEGORY     VARCHAR(250),
+    TIME         INTEGER,
+    TABLE_NUMBER INTEGER
 );
 
